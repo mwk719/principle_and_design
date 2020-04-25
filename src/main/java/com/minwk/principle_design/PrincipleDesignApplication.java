c531dca3756f8020c19d8e5abd23073653c3ca51
minweikai <1294608448@qq.com>
2020-04-25 15:57:14 +0800

    add 初始化

---
+++ src/main/java/com/minwk/principle_design/PrincipleDesignApplication.java
@@ -0,0 +1,13 @@
+package com.minwk.principle_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipleDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipleDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minwk/principle_design/PrincipleDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minwk.principle_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipleDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
